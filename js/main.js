09204c8341215ccb5b90f05927f3e16f89b42896
costelo01 <ajg_costelo@hotmail.com>
2018-06-15 17:10:17 +0800

    version 1.0

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+let properties = {
+		line : window.innerHeight || document.body.clientHeigh,
+		column : window.innerWidth || document.body.clientWidth,
+		tabs : 4
+}
+
+let behavior = {	
+
+
+
+}
+
+let arrayLength = [[],[]]
+
+
+
+$('.window').append("<span id='text'><img width = '20' height = '20'"+
+	"src='https://ih1.redbubble.net/image.428914186.0177/flat,800x800,075,f.u1.jpg'> &nbsp"+ 
+	"C:Users\\Aj\\Desktop\\p\\index.html (p) - Sublime Text (UNREGISTERED)</span>");
+
+$('.nav').append('<ul>'+
+        '<li><a href="#">File</a></li>'+
+        '<li><a href="#">Edit</a></li>'+
+        '<li><a href="#">Selection</a></li>'+
+        '<li><a href="#">Find</a></li>'+
+        '<li><a href="#">View</a></li>'+
+        '<li><a href="#">Goto</a></li>'+
+        '<li><a href="#">Tools</a></li>'+
+        '<li><a href="#">Project</a></li>'+
+        '<li><a href="#">Preferences</a></li>'+
+        '<li><a href="#">Help</a></li>'+
+      '</ul>');
+
