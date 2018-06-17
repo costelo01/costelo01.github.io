30bb1df3937bd58025b446f1ba28028879559ab4
costelo01 <ajg_costelo@hotmail.com>
2018-06-17 17:06:56 +0800

    updated css, mdfd menu, dropdown_.js

---
+++ js/dropdown_.js
@@ -0,0 +1,56 @@
+var dropdown_ = {
+    "File" : {
+        "New File" : "Ctrl+N",
+        "Open File..." : "Ctrl+O",
+        "Open Folder..." : "",
+        "Open Recent" : [],
+        "Reopen with Encoding" : [],
+        "New View into File" : "",
+        "Save" : "",
+        "Save with Encoding" : "",
+        "Save As..." : "Ctrl+Shift+S",
+        "Save All" : "",
+        "<span class='line'></span>" : "",
+        "New Window" : "",
+        "Close Window" : "",
+        "<span class='line1'></span>" : "",
+        "Close File" : "",
+        "Revert File" : "",
+        "Close All Files" : "",
+        "<span class='line2'></span>" : "",
+        "Exit" : ""
+
+    },
+    "Edit" : {
+        "Undo Insert Characters" : "Ctrl+Z",
+        "Repeat Insert Character" : "Ctrl+Y",
+        "Undo Selection" : [],
+        "<span class='line'></span>" : "",
+        "Copy" : "",
+        "Cut" : "",
+        "Paste" : "",
+        "Paste and Indent" : "",
+        "Paste from History" : "",
+        "<span class='line1'></span>" : "",
+        "Line" : "",
+        "Comment" : "",
+        "Text" : "",
+        "Tag" : "",
+        "Code Folding" : "",
+        "Convert Case" : "",
+        "Wrap" : "",
+        "Show Completions" : "Ctrl+Space",
+        "<span class='line2'></span>" : "",
+
+    },
+    "Selection" : "",
+    "Find" : "",
+    "View" : "",
+    "Goto" :"",
+    "Tools" :"",
+    "Project" :"",
+    "Preferences" : "",
+    "Help" : ""
+
+
+}
--- js/main.js
+++ js/main.js
@@ -5,38 +5,78 @@ let properties = {
 		tabs : 4
 }
 
-let behavior = {	
-
+let behaviors = {	
 
 
 }
 
+
 let arrayLength = [[],[]]
 
-
-
-$('.window').append("<span id='text'><img width = '20' height = '20'"+
+//window
+$('.window').html("<span id='text'><img width = '20' height = '20'"+
 	"src='https://ih1.redbubble.net/image.428914186.0177/flat,800x800,075,f.u1.jpg'> &nbsp"+ 
 	"C:Users\\Aj\\Desktop\\p\\index.html (p) - Sublime Text (UNREGISTERED)</span>");
 
-$('.nav').append('<ul>'+
-        '<li><a href="#">File</a></li>'+
-        '<li><a href="#">Edit</a></li>'+
-        '<li><a href="#">Selection</a></li>'+
-        '<li><a href="#">Find</a></li>'+
-        '<li><a href="#">View</a></li>'+
-        '<li><a href="#">Goto</a></li>'+
-        '<li><a href="#">Tools</a></li>'+
-        '<li><a href="#">Project</a></li>'+
-        '<li><a href="#">Preferences</a></li>'+
-        '<li><a href="#">Help</a></li>'+
+
+//dropdown menu
+$('.nav').html('<ul id ="nav">'+
+        '<li><a>File</a><div class="dropdown-content" id="File">'+
+            ' </div></li><li><a>Edit</a><div class="dropdown-content" id="Edit"><span class="space"></span>'+
+            ' </div></li>'+
+        '<li><a>Selection</a><div class="dropdown-content" id="Selection">'+
+            ' </div></li>'+
+        '<li><a>Find</a><div class="dropdown-content" id="Find">'+
+            ' </div></li>'+
+        '<li><a>View</a><div class="dropdown-content" id="View">'+
+            ' </div></li>'+
+        '<li><a>Goto</a><div class="dropdown-content" id="Goto">'+
+            ' </div></li>'+
+        '<li><a>Tools</a><div class="dropdown-content" id="Tools">'+
+            ' </div></li>'+
+        '<li><a>Project</a><div class="dropdown-content" id="Project">'+
+            ' </div></li>'+
+        '<li><a>Preferences</a><div class="dropdown-content" id="Preferences">'+
+            ' </div></li>'+
+        '<li><a>Help</a><div class="dropdown-content" id="Help">'+
+            ' </div></li>'+
       '</ul>');
 
 $('.child').resizable({
-    handles: 'w,e',
+    handles: 'w',
     minWidth: 60,
-    maxWidth: window.innerWidth - 60
+    maxWidth: document.body.clientWidth - 60
 });
 
+
+$.each(dropdown_ , function(key, value){
+   
+
+    //
+
+    //console.log()
+})
+
+    
+
+//tab menu
+for(let i in dropdown_){
+    const key = [];
+    const value = [];
+
+    if (!dropdown_.hasOwnProperty(i)) {
+        continue;
+    }
+    for(let o in dropdown_[i]){
+        key.push(o,'<br>')
+        value.push(dropdown_[i][o])
+    }
+     $('#'+i).html(key);
+     
+}
+
+
 // $('.nav-tab').append('<br><span style="font-weight:bold;color:white;font-family:calibri;">FOLDERS</span>');
+
+
 
