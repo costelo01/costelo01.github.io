0f9e129217983e011a0706f797e22e041d2fd57b
costelo01 <ajg_costelo@hotmail.com>
2018-07-12 17:50:32 +0800

    sidebar

--- js/main.js
+++ js/main.js
@@ -1,82 +1,75 @@
 //render partials
 //window
-$('.window').html("<span id='text'><img width = '20' height = '20'"+
-  "src='https://ih1.redbubble.net/image.428914186.0177/flat,800x800,075,f.u1.jpg'> &nbsp"+ 
-  "C:Users\\Aj\\Desktop\\p\\index.html (p) - Sublime Text (UNREGISTERED)</span>");
+jQuery('.window').html("<span id='text'><img width = '20' height = '20'"+
+"src='https://ih1.redbubble.net/image.428914186.0177/flat,800x800,075,f.u1.jpg'> &nbsp"+ 
+"C:Users\\Aj\\Desktop\\p\\index.html (p) - Sublime Text (UNREGISTERED)</span>");
 
 
 //dropdown menu
-$('.nav').html('<ul id ="nav">'+
-        '<li><a>File</a><div class="dropdown-content" id="File">'+
-            ' </div></li><li><a>Edit</a><div class="dropdown-content" id="Edit">'+
-            ' </div></li>'+
-        '<li><a>Selection</a><div class="dropdown-content" id="Selection">'+
-            ' </div></li>'+
-        '<li><a>Find</a><div class="dropdown-content" id="Find">'+
-            ' </div></li>'+
-        '<li><a>View</a><div class="dropdown-content" id="View">'+
-            ' </div></li>'+
-        '<li><a>Goto</a><div class="dropdown-content" id="Goto">'+
-            ' </div></li>'+
-        '<li><a>Tools</a><div class="dropdown-content" id="Tools">'+
-            ' </div></li>'+
-        '<li><a>Project</a><div class="dropdown-content" id="Project">'+
-            ' </div></li>'+
-        '<li><a>Preferences</a><div class="dropdown-content" id="Preferences">'+
-            ' </div></li>'+
-        '<li><a>Help</a><div class="dropdown-content" id="Help">'+
-            ' </div></li>'+
-      '</ul>');
+jQuery('.nav').html('<ul id ="nav">'+
+'<li><a>File</a><div class="dropdown-content" id="File">'+
+'</div></li><li><a>Edit</a><div class="dropdown-content" id="Edit">'+
+'</div></li>'+
+'<li><a>Selection</a><div class="dropdown-content" id="Selection">'+
+'</div></li>'+
+'<li><a>Find</a><div class="dropdown-content" id="Find">'+
+'</div></li>'+
+'<li><a>View</a><div class="dropdown-content" id="View">'+
+'</div></li>'+
+'<li><a>Goto</a><div class="dropdown-content" id="Goto">'+
+'</div></li>'+
+'<li><a>Tools</a><div class="dropdown-content" id="Tools">'+
+'</div></li>'+
+'<li><a>Project</a><div class="dropdown-content" id="Project">'+
+'</div></li>'+
+'<li><a>Preferences</a><div class="dropdown-content" id="Preferences">'+
+'</div></li>'+
+'<li><a>Help</a><div class="dropdown-content" id="Help">'+
+'</div></li>'+
+'</ul>');
 
 //content
-$('.content').html('<div class = "parent">'+
-       '<div style="position: absolute;'+
-          'left: 0px;'+
-          'top: 0px;'+
-          'color: #b7b7b7;'+
-          'z-index: -1;">Folders'+
-      
-              '<div class="file-folder" style="z-index: -1;">p</div>'+  //file folder
-
-
-              '</div>'+
-
-      '<div class = "child">'+
-           '<div class="tab"><div class="code-content"></div></div>'+
-      '</div>'+
-    '</div>');
+jQuery('.content').html('<div class = "parent">'+
+'<div style="position: absolute;'+
+'left: 0px;'+
+'top: 10px;'+
+'color: #b7b7b7;'+
+'z-index: -1;"><span class="myfol" style="font-weight: bolder; padding: 15px;'+ 
+    'font-family: Trebuchet MS; text-transform: uppercase; font-size: 15px; cursor: context-menu;">Folders</span>'+
+'<div class="file-folder" style="z-index: -1;"></div>'+  //file folder
+'</div>'+
+'<div class = "child">'+
+'<div class="tab"><div class="code-content"></div></div>'+
+'</div>'+
+'</div>');
 
 
 
-
-$('#nav > li > a').on('click focus', function(){
-
-  var period_val = $(this).text();
-
-  $('#'+period_val).css( "display", "block" );
-
-  $('#nav > li ').mouseover(function() {
-    $(this).find(" > div").css( "display", "block" );
-    }).mouseout(function() {
-      $(this).find(" > div").css( "display", "none" );
+jQuery('.file-folder').html('<span></span>');
 
 
-  });
 
-  $(this).on('dblclick ', function(){
 
-    $('#'+period_val).css( "display", "none" );
+jQuery('#nav > li > a').on('click focus', function(){
+    var period_val = jQuery(this).text();
+    jQuery('#'+period_val).css( "display", "block" );
 
-    $('#nav > li ').mouseover(function() {
-       $(this).find(" > div").css( "display", "none" );
+    jQuery('#nav > li ').mouseover(function() {
+
+      jQuery(this).find(" > div").css( "display", "block" );
+
+      }).mouseout(function() {
+        jQuery(this).find(" > div").css( "display", "none" );
+
+    });
+
+    jQuery(this).on('dblclick ', function(){
+      jQuery('#'+period_val).css( "display", "none" );
+      jQuery('#nav > li ').mouseover(function() {
+         jQuery(this).find(" > div").css( "display", "none" );
+      })
     })
 
-
-  })
-
-
-
-  
 });
 
 
@@ -94,14 +87,23 @@ let behaviors = {
 let arrayLength = [[],[]]
 
 
-$('.child').resizable({
+jQuery('.child').resizable({
     handles: 'w',
     minWidth: 55,
-    maxWidth: window.innerWidth - 55
+    maxWidth: window.innerWidth - 65
 });
 
-
+const dropdown_={File:{"New File":"Ctrl+N","Open File...":"Ctrl+O","Open Folder...":"","Open Recent":[],"Reopen with Encoding":[],"New View into File":"",Save:"","Save with Encoding":"","Save As...":"Ctrl+Shift+S","Save All":"","<span class='line'><hr></span>":"","New Window":"Ctrl+Shift+N","Close Window":"Ctrl+Shift+W","<span class='line1'><hr></span>":"","Close File":"Ctrl+W","Revert File":"","Close All Files":"","<span class='line2'><hr></span>":"",Exit:""},Edit:{"Undo Insert Characters":"Ctrl+Z",
+"Repeat Insert Character":"Ctrl+Y","Undo Selection":[],"<span class='line'><hr></span>":"",Copy:"",Cut:"",Paste:"","Paste and Indent":"","Paste from History":"","<span class='line1'><hr></span>":"",Line:"",Comment:"",Text:"",Tag:"","Code Folding":"","Convert Case":"",Wrap:"","Show Completions":"Ctrl+Space","<span class='line2'><hr></span>":"","Sort Lines":"","Sort Lines (Case Sensitive)":"","Permute Lines":"","Permute Selections":""},Selection:{"Split into Lines":"Ctrl+Shift+L","Add Previous Line":"Ctrl+Alt+Up",
+"Add Next Line":"Ctrl+Alt+Down","Single Selection":"Escape","Invert Selection":"","<span class='line'><hr></span>":"","Select All":"Ctrl+A","Expand Selection to Line":"Ctrl+L","Expand Selection to Word":"Ctrl+D","Expand Selection to Paragraph":"","Expand Selection to Scope":"Ctrl+Shift+Space","Expand Selection to Brackets":"Ctrl+Shift+M","Expand Selection to Indentation":"Ctrl+Shift+J","Expand Selection to Tag":"Ctrl+Shift+A"},Find:{"Find...":"Ctrl+F","Find Next":"F3","Find Previous":"Shift+F3","Incremental Find":"Ctrl+I",
+"<span class='line'><hr></span>":"","Replace...":"Ctrl+H","Replace Next":"Ctrl+Shift+N","<span class='line1'><hr></span>":"","Quick Find":"Ctrl+F3","Quick Find All":"Alt+F3","Quick Add Next":"Ctrl+D","Quick Skip Next":"Ctrl+K,Ctrl+D","<span class='line2'><hr></span>":"","Use Selection for Find":"Ctrl+E","Use Selection for Replace":"Ctrl+Shift+E","<span class='line3'><hr></span>":"","Find in Files...":"Ctrl+Shift+F","Find Results":[]},View:{"Side Bar":"","Hide Minimap":"","Hide Tabs":"","Hide Status Bar":"",
+"Hide Menu":"","Show Console":"Ctrl+`","<span class='line'><hr></span>":"","Enter Full Screen":"F11","Enter Full Distraction Mode":"Shift+F11","<span class='line1'><hr></span>":"",Layout:"",Groups:"","Focus Groups":"","Move File to Group":"","<span class='line2'><hr></span>":"",Syntax:"",Indentation:"","Line Endings":"","<span class='line3'><hr></span>":"","Word Wrap":"","Word Wrap Column":"",Ruler:"","<span class='line4'><hr></span>":"","Spell Check":"F6","Next Misspelling":"","Previous Misspelling":"",
+Dictionary:""},Goto:{"Goto Anything...":"Ctrl+P","<span class='line'><hr></span>":"","Goto Symbol...":"Ctrl+R","Goto Symbol in Project...":"Ctrl+Shift+R","Goto Definition...":"F12","Goto Reference...":"Shift+F12","Goto Line...":"Ctrl+G","<span class='line2'><hr></span>":"","Jump Back":"Alt+-","Jump Foward":"Alt+Shift+-","<span class='line3'><hr></span>":"","Switch File":"","<span class='line4'><hr></span>":"",Scroll:"",Bookmarks:"","<span class='line5'><hr></span>":"","Jump to Matching Bracket":"Ctrl+M"},
+Tools:{"Command Palette...":"Ctrl+Shift+P","Snippets...":"","<span class='line'><hr></span>":"","Build System":"",Build:"Ctrl+B","Build With...":"Ctrl+Shift+B","Cancel Build":"Ctrl+Break","Build Results":"","Save All on Build":"","<span class='line2'><hr></span>":"","Record Macro":"Ctrl+Q","Playback Macro":"Ctrl+Shift+Q","Save Macro...":"",Macros:"","<span class='line3'><hr></span>":"",Developer:"","Install Package Control":""},Project:{"Open Project...":"","Switch Project...":"","Quick Switch Project...":"",
+"Open Recent":"","<span class='line'><hr></span>":"","Save Project As...":"","Close Project":"","Edit Project":"","<span class='line1'><hr></span>":"","New Workspace for Project":"","Save Workspace As...":"","<span class='line2'><hr></span>":"","Add Folder to Project...":"","Remove all Folders from Project":"","Refresh Folders":""},Preferences:{"Browse Packages":"","<span class='line'><hr></span>":"",Settings:"","Settings\u2013Syntax Specific":"","Settings\u2013Distraction Free":"","<span class='line2'><hr></span>":"",
+"Key Bindings":"","<span class='line3'><hr></span>":"","Color Scheme...":"","Theme...":"",Font:""},Help:{Documentation:"",Twitter:"","<span class='line'><hr></span>":"","Indexing Status":"","<span class='line1'><hr></span>":"","Purchase License":"","Enter License":"","<span class='line2'><hr></span>":"","Check for Updates...":"","Changelog...":"","Me":""}};
 //tab menu
+
 for(let i in dropdown_){
     const key = [];
     const value = [];
@@ -114,13 +116,92 @@ for(let i in dropdown_){
           '<span class="value">'+dropdown_[i][o]+'<span></div>')
         value.push(dropdown_[i][o])
     }
-     $('#'+i).html(key)
-     //$('#'+i).css('padding-left','35px')
+     jQuery('#'+i).html(key)
      
 }
 
 
-// $('.nav-tab').append('<br><span style="font-weight:bold;color:white;font-family:calibri;">FOLDERS</span>');
+const file_icons = {
+
+  php : '<i class="fab fa-php"></i>',
+  js : '<i class="fab fa-js"></i>',
+  dotfiles : "",
+  txtfiles : "",
+  image : '<i class="far fa-image"></i>',
+  markup : '<i class="fas fa-code"></i>',
+  manifest: "",
+  folder : '<i class="far fa-folder"></i>'
+  
+}
+
+const function__ = {
+
+  folder_c : {"New File" : "",
+  "Rename...": "",
+  "<span class='line'><hr></span>": "",
+  "New Folder": "",
+  "Delete Folder": "","Find in Folder...": ""},
+
+  file_c : {"Rename...": "","Delete File": "","Open Containing Folder...": ""},
+
+  folder :  function(){
+
+    
+
+
+  },
+
+  file:  function(){
+
+
+
+
+  }
+  
+}
+
+
+let folders_files = {
+
+    "me" : {"school.js": "","achievements.php": "","life.html":""}, 
+    "img" : {},
+    "css" : {},
+    "js" : {}
+
+}
+
+
+// sort algo: https://stackoverflow.com/questions/5467129/sort-javascript-object-by-key
+let ksort = function ( src ) {
+      var keys = Object.keys( src ),
+          target = {};
+      keys.sort();
+
+      keys.forEach(function ( key ) {
+        target[ key ] = src[ key ];
+      });
+
+      return target;
+};
+
+
+for(let i in ksort(folders_files)){
+
+  if (!folders_files.hasOwnProperty(i)) {
+          continue;
+  }
+
+  jQuery('.file-folder').append('<p><span class="icoo">'+file_icons.folder+'</span> '+i+'</p>')
+
+  for(let o in ksort(folders_files[i])){
+      
+  }
+  
+}
+
+
+
+
 
 
 // $('.key').on('click', function () {
